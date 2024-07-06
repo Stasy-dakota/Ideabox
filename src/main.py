5c7636d57d57db92bc3eb6d93a3bb3a2506b3f0b
Анастасия Нестерова <nastya-nesterova@yandex.ru>
2024-07-07 01:21:30 +0300

    Added function for transforming time and masking card and account

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from masks import get_mask_account, get_mask_card_number
+from widget import get_date, mask_account_card
 
 print(get_mask_account(1234567890123456))
 print(get_mask_card_number(23654890436512183622))
+
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счёт 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,18 @@
+
+from typing import Any
+
+import masks
+
+
+def mask_account_card(mask_card_and_account: Any) -> Any:
+    """Функция маскировки банковской карты и банковского счёта"""
+    if "Счёт" in mask_card_and_account:
+        return masks.get_mask_account(mask_card_and_account)
+    else:
+        cards = masks.get_mask_card_number(mask_card_and_account[-16:])
+        return cards
+
+
+def get_date(date: str) -> str:
+    """Функция трансформации даты"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
