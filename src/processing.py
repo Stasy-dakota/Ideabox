629159d39b8226a117ff3c341fdff4879ee37dbe
Анастасия Нестерова <nastya-nesterova@yandex.ru>
2024-07-12 16:24:52 +0300

    Homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -12,6 +12,6 @@ def filter_by_state(base_list: list[dict[str, Any]], state_id: str = "EXECUTED")
 
 def sort_by_date(base_list: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
     """Функция сортировки исходных данных по дате"""
-    sorted_base_list = sorted(base_list, key = lambda base_list: base_list["date"], reverse=True)
+    sorted_base_list = sorted(base_list, key=lambda base_list: base_list["date"], reverse=True)
     return sorted_base_list
 
